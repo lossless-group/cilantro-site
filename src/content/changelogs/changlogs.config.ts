80a6d7ef42fc23652f39d52e54e01b5dc95b7a3d
mpstaton <mpstaton@gmail.com>
2025-10-14 17:55:30 +0200

    update(collection): update collection model for changelog
     On branch main
     Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/content/changelogs/changelog-cilantro-site
    	modified:   src/content/changelogs/changlogs.config.ts

--- src/content/changelogs/changlogs.config.ts
+++ src/content/changelogs/changlogs.config.ts
@@ -4,20 +4,28 @@ import { z, defineCollection } from 'astro:content';
   const changelogSchema = z.object({
     version: z.string().optional(),
     title: z.string(),
-    date: z.date(),
-    product: z.enum(['neo', 'parslee', 'site']),
-    type: z.enum(['major', 'minor', 'patch', 'documentation', 'refactor',
-  'infrastructure']),
-    published: z.boolean().default(true),
+    lede: z.string().optional(),
+    slug: z.string(),
+    date_created: z.date().optional(),
+    date_modified: z.date().optional(),
+    date_started: z.date().optional(),
+    date_shipped: z.date().optional(),
+    product: z.enum(['Neo', 'Parslee', 'Cilantro-Site']),
+    type: z.enum(['Major', 'Minor', 'Patch', 'Documentation', 'Refactor',
+  'Infrastructure']),
+    is_release: z.boolean().optional().default(false),
+    publish: z.boolean().default(true),
     summary: z.string().optional(),
-    author: z.string().optional(),
+    why_care: z.string().optional(),
+    authors: z.array(z.string()).optional(),
+    contributors: z.array(z.string()).optional(),
     tags: z.array(z.string()).optional(),
-  });
+  }); 
 
   export const changelogNeo = defineCollection({
     loader: glob({
       pattern: '**/*.md',
-      base: './src/content/changelogs/neo'
+      base: './src/content/changelogs/changelog_neo'
     }),
     schema: changelogSchema.extend({
       product: z.literal('neo'),
@@ -27,7 +35,7 @@ import { z, defineCollection } from 'astro:content';
   export const changelogParslee = defineCollection({
     loader: glob({
       pattern: '**/*.md',
-      base: './src/content/changelogs/parslee'
+      base: './src/content/changelogs/changelog_parslee'
     }),
     schema: changelogSchema.extend({
       product: z.literal('parslee'),
@@ -37,7 +45,7 @@ import { z, defineCollection } from 'astro:content';
   export const changelogSite = defineCollection({
     loader: glob({
       pattern: '**/*.md',
-      base: './src/content/changelogs/site'
+      base: './src/content/changelogs/changelog_cilantro_site'
     }),
     schema: changelogSchema.extend({
       product: z.literal('site'),
